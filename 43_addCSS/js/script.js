d662ed50c0f18c258bedc0677af9d7c0a1f94be6
Vitor Muneretto Tinelli <vitor.mtinelli@gmail.com>
2023-04-02 16:11:04 -0300

    fifteenthDay

---
+++ 43_addCSS/js/script.js
@@ -0,0 +1,7 @@
+var title = document.querySelector("#title");
+
+//add style
+title.style.color = "red"
+
+//background-color
+title.style.backgroundColor = "yellow";
---
+++ 44_docProperty/js/script.js
@@ -0,0 +1,15 @@
+//propriedades doc
+
+console.log(document.body);
+console.log(document.head);
+console.log(document.links[0]);
+
+document.links[0].textContent = "Twitter";
+
+
+console.log(document.URL);
+console.log(document.title);
+
+
+document.title = "Aula 44";
+console.log(document.title);
